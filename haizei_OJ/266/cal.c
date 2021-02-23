59eeefeab28ee283eda855ab81fc307b7a52be52
Elzat Kurbanjan <m15150503973@163.com>
2021-02-23 13:01:11 +0000

    [haizei_OJ] finish problem 266

--- haizei_OJ/266/cal.c
+++ haizei_OJ/266/cal.c
@@ -32,45 +32,69 @@ int single_cal(char op, int a, int b) {
 int calc(char *exp) {
     int i = 0;
     int opr = 0;
-    // int op_flag = 0;    // 用于区分加减号与正负号
-    int in_priority[256] = {0};
-    // int out_priority[256] = {0};
+    int op_flag = 0;    // 用于区分加减号与正负号
+    int priority[256] = {0};
     int tmp_val = 0;
+    char tmp_operator = 0;
 
-    in_priority['+'] = 3;
-    in_priority['-'] = 3;
-    in_priority['*'] = 6;
-    in_priority['/'] = 6;
-    in_priority['^'] = 9;
+    priority['+'] = 3;
+    priority['-'] = 3;
+    priority['*'] = 6;
+    priority['/'] = 6;
+    priority['^'] = 9;
 
     while (exp[i]) {
         //printf("%c i: %d\n", exp[i], i);
         switch (exp[i]) {
             case '+' : 
-            case '-' :
+            case '-' : {
+                /* 正负号处理 */
+                if (op_flag) {
+                    tmp_operator = exp[i++];
+                    opr = get_num(exp, &i);
+                    opr = tmp_operator == '-' ? -opr : opr;
+                    operand[++operand_top] = opr;
+                    op_flag = 0;
+                    break;
+                }
+            }
             case '*' :
             case '/' : 
             case '^' :{
-                if ((-1 != operator_top) && (in_priority[operator[operator_top]] > in_priority[exp[i]])) {
+                op_flag = 1;
+                while ((-1 != operator_top) && (priority[operator[operator_top]] >= priority[exp[i]])) {
                     tmp_val = single_cal(operator[operator_top], operand[operand_top - 1], operand[operand_top]);
-                    printf("%d %c %d = %d\n", operand[operand_top - 1], operator[operator_top], operand[operand_top], tmp_val);
                     operator_top -= 1;
                     operand_top -= 2;
                     operand[++operand_top] = tmp_val;
                 }
                 operator[++operator_top] = exp[i];
             } break;
+            case '(' : {
+                op_flag = 1;
+                operator[++operator_top] = exp[i];
+            } break;
+            case ')' : {
+                while (operator[operator_top] != '(') {
+                    tmp_val = single_cal(operator[operator_top], operand[operand_top - 1], operand[operand_top]);
+                    operator_top -= 1;
+                    operand_top -= 2;
+                    operand[++operand_top] = tmp_val;
+                }
+                op_flag = 0;
+                --operator_top;
+            } break;
             default : {
                 opr = get_num(exp, &i);
                 operand[++operand_top] = opr;
+                op_flag = 0;
             } 
         }
         ++i;
     }
 
-    while (-1 != operator_top) {
+    while (-1 != operator_top && operand_top != 0) {
         tmp_val = single_cal(operator[operator_top], operand[operand_top - 1], operand[operand_top]);
-        printf("%d %c %d = %d\n", operand[operand_top - 1], operator[operator_top], operand[operand_top], tmp_val);
         operator_top -= 1;
         operand_top -= 2;
         operand[++operand_top] = tmp_val;
--- haizei_OJ/266/test.sh
+++ haizei_OJ/266/test.sh
@@ -1,4 +1,6 @@
 for line in `cat test_data`;do
+	echo expr: ${line}
 	echo ${line} | ./a.out
-	echo expected: ${line} = $((${line}))
+	echo
+	#echo expected: ${line} = $((${line}))
 done
