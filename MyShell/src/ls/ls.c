4c205013c7bd14dcec3741bbca54311e304e2051
Elzat Kurbanjan <m15150503973@163.com>
2021-03-26 07:19:00 +0000

    [MyShell] initial MyShell project

---
+++ MyShell/src/ls/ls.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+typedef enum _flags {
+    LIST = 0,
+    ALL
+} FLAGS;
+
+typedef enum _error {
+    OK = 0,
+    EARG
+} ERRORS;
+
+void usage() {
+    printf("ls [-a] [-l] [dirname...]\n");
+    return;
+}
+
+int resolve_cmdline_args(int argc, char * argv[], int *flags) {
+    char opt = 0;
+
+    while ((opt = getopt(argc, argv, "al"))  != -1) {
+        switch (opt)
+        {
+        case 'a':
+            (*flags) |= (1 << ALL);
+            break;
+        
+        case 'l':
+            (*flags) |= (1 << LIST);
+            break;
+
+        default:
+            usage();
+            return EARG;
+        }
+    }
+
+    return OK;
+}
+
+int run(int flags) {
+    
+
+    return 0;
+} 
+
+int main(int argc, char * argv[]) {
+    int flags = 0;
+    int ret = 0;
+
+    ret = resolve_cmdline_args(argc, argv, &flags);
+    if (OK != ret) {
+        return ret;
+    }
+
+    return run(flags);;
+}
